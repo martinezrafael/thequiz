6278bbbd9ffa5767530673623cc294c34542cc2e
Rafael Molina Martinez <rafaelmartinez.contato@gmail.com>
2022-06-23 19:13:22 -0300

    criando uma função que cria as perguntas

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -1,78 +1,107 @@
 "use strict"; 
 
-//questions
+const createQuestion = (title, description, answers, theme, dificultty) => {
+    return {
+        title: title,
+        description: description,
+        answers: answers,
+        theme: theme,
+        dificultty: dificultty
+    }
+}
+
 const questions = [
-    {
-        title: "Pergunta 1",
-        description: "Descrição da pergunta 1",
-        answers: ["opção 1", "opção 2", "opção 3"],
-        theme: "music",
-        dificultty: "easy",
-    },
-    {
-        title: "Pergunta 2",
-        description: "Descrição da pergunta 2",
-        answers: ["opção 1", "opção 2", "opção 3"],
-        theme: "music",
-        dificultty: "medium",
-    },
-    {
-        title: "Pergunta 3",
-        description: "Descrição da pergunta 3",
-        answers: ["opção 1", "opção 2", "opção 3"],
-        theme: "music",
-        dificultty: "hard",
-    },
-    {
-        title: "Pergunta 4",
-        description: "Descrição da pergunta 4",
-        answers: ["opção 1", "opção 2", "opção 3"],
-        theme: "movie",
-        dificultty: "easy",
-    },
-    {
-        title: "Pergunta 5",
-        description: "Descrição da pergunta 5",
-        answers: ["opção 1", "opção 2", "opção 3"],
-        theme: "movie",
-        dificultty: "medium",
-    },
-    {
-        title: "Pergunta 6",
-        description: "Descrição da pergunta 6",
-        answers: ["opção 1", "opção 2", "opção 3"],
-        theme: "movie",
-        dificultty: "hard",
-    },
-    {
-        title: "Pergunta 7",
-        description: "Descrição da pergunta 7",
-        answers: ["opção 1", "opção 2", "opção 3"],
-        theme: "knowledge",
-        dificultty: "easy",
-    },
-    {
-        title: "Pergunta 8",
-        description: "Descrição da pergunta 8",
-        answers: ["opção 1", "opção 2", "opção 3"],
-        theme: "knowledge",
-        dificultty: "medium",
-    },
-    {
-        title: "Pergunta 9",
-        description: "Descrição da pergunta 9",
-        answers: ["opção 1", "opção 2", "opção 3"],
-        theme: "knowledge",
-        dificultty: "hard",
-    },
-    {
-        title: "Pergunta 10",
-        description: "Descrição da pergunta 10",
-        answers: ["opção 1", "opção 2", "opção 3"],
-        theme: "knowledge",
-        dificultty: "hard",
-    },
+    createQuestion(
+        "Pergunta 1",
+        "Descrição da pergunta 1",
+        ["opção 1", "opção 2", "opção 3"],
+        "Música",
+        "Fácil"
+    ),
+    createQuestion(
+        "Pergunta 2",
+        "Descrição da pergunta 2",
+        ["opção 1", "opção 2", "opção 3"],
+        "Música",
+        "Médio"
+    ),
+    createQuestion(
+        "Pergunta 3",
+        "Descrição da pergunta 3",
+        ["opção 1", "opção 2", "opção 3"],
+        "Música",
+        "Difícil"
+    ),
+    createQuestion(
+        "Pergunta 4",
+        "Descrição da pergunta 4",
+        ["opção 1", "opção 2", "opção 3"],
+        "Filmes",
+        "Fácil"
+    ),
+    createQuestion(
+        "Pergunta 5",
+        "Descrição da pergunta 5",
+        ["opção 1", "opção 2", "opção 3"],
+        "Filmes",
+        "Médio"
+    ),
+    createQuestion(
+        "Pergunta 6",
+        "Descrição da pergunta 6",
+        ["opção 1", "opção 2", "opção 3"],
+        "Filmes",
+        "Difícil"
+    ),
+    createQuestion(
+        "Pergunta 7",
+        "Descrição da pergunta 7",
+        ["opção 1", "opção 2", "opção 3"],
+        "Conhecimentos Gerais",
+        "Fácil"
+    ),
+    createQuestion(
+        "Pergunta 8",
+        "Descrição da pergunta 8",
+        ["opção 1", "opção 2", "opção 3"],
+        "Conhecimentos Gerais",
+        "Médio"
+    ),
+    createQuestion(
+        "Pergunta 9",
+        "Descrição da pergunta 9",
+        ["opção 1", "opção 2", "opção 3"],
+        "Conhecimentos Gerais",
+        "Difícil"
+    ),
+    createQuestion(
+        "Pergunta 10",
+        "Descrição da pergunta 10",
+        ["opção 1", "opção 2", "opção 3"],
+        "Conhecimentos Gerais",
+        "Difícil"
+    ),
+    createQuestion(
+        "Pergunta 11",
+        "Descrição da pergunta 11",
+        ["opção 1", "opção 2", "opção 3"],
+        "Conhecimentos Gerais",
+        "Fácil"
+    ),
+    createQuestion(
+        "Pergunta 12",
+        "Descrição da pergunta 12",
+        ["opção 1", "opção 2", "opção 3"],
+        "Conhecimentos Gerais",
+        "Fácil"
+    ),
 ];
+
+
+
+const upper = (str) => {
+    return str.toUpperCase();
+}
 
 const createArrayDOM = (className) => {
     return document.querySelectorAll(className);
@@ -88,6 +117,7 @@ const getItemArrDOM = (arr) => {
         if (item.selected) {
             itemSelected = item.value;
         }
+
     })
 
     return itemSelected;
@@ -106,7 +136,6 @@ const filterArr = (arr) => {
     return newArr;
 }
 
-
 const renderArr = () => {
     let newArr = filterArr(questions);
     let container = document.getElementById('questions__container')
@@ -118,18 +147,23 @@ const renderArr = () => {
                 <p class='question__description'>${element.description}</p>
                 <ul class='question__answers'>
                     ${element.answers.map(answer => {
-                        return`<li class='question__answer'><a href='#'>${answer}</a></li>`
+                        return `<li class='question__answer'><a href='#'>${answer}</a></li>`
                     })}
                 </ul>
                 <div class='question__footer'>
-                    <span class='question__tag'>${element.theme}</span>
-                    <span class='question__tag'>${element.dificultty}</span>
+                    <span class='question__tag'>${upper(element.theme)}</span> |
+                    <span class='question__tag'>${upper(element.dificultty)}</span>
                 </div>
             </div>
         `
     })
        
 }
+
+// questions.forEach(question => {
+//     question.dificultty = 'default';
+//     console.log(question);
+// })
 
 
 const play = document.getElementById("btn__play");
