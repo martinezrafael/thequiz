37dc96bdda0892a32a96ff91b30407049d724141
Rafael Molina Martinez <rafaelmartinez.contato@gmail.com>
2022-06-28 17:11:29 -0300

    automatizando a funão que verifica se a resposta clicada é a correta

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -2,30 +2,29 @@
 
 const questions = [
     {
-        title: 'Pergunta 1',
+        title: 'Pergunta 1 - Música',
         description: 'Descrição da Pergunta 1',
         answers:['opcao 1','opcao 2','opcao 3'],
         theme: 'music',
         dificultty: 'default',
-        correctAnswer: 'alternative-0'
-
+        correctAnswer: 'alternative-0',
     },
     {
-        title: 'Pergunta 2',
+        title: 'Pergunta 2 - Música',
         description: 'Descrição da Pergunta 2',
         answers:['opcao 4','opcao 5','opcao 6'],
         theme: 'music',
         dificultty: 'default',
-        correctAnswer: 'alternative-1'
+        correctAnswer: 'alternative-2'
 
     },
     {
-        title: 'Pergunta 3',
+        title: 'Pergunta 3 - Música',
         description: 'Descrição da Pergunta 3',
         answers:['opcao 7','opcao 8','opcao 9'],
         theme: 'music',
         dificultty: 'default',
-        correctAnswer: 'alternative-2'
+        correctAnswer: 'alternative-3'
 
     },
     {
@@ -34,7 +33,16 @@ const questions = [
         answers:['opcao 10','opcao 11','opcao 12'],
         theme: 'music',
         dificultty: 'default',
-        correctAnswer: 'alternative-2'
+        correctAnswer: 'alternative-1'
+
+    },
+    {
+        title: 'Pergunta 4',
+        description: 'Descrição da Pergunta 3',
+        answers:['opcao 10','opcao 11','opcao 12'],
+        theme: 'music',
+        dificultty: 'default',
+        correctAnswer: 'alternative-1'
 
     },
 ]
@@ -96,11 +104,22 @@ const checkAnswer = (event) => {
 
     let targetClass = event.target.className;
 
-    let questionTarget = targetClass.split(' ')[1];
-    let answerTarget = targetClass.split(' ')[2];
+    let targetQuestionClass = Number(targetClass.split(' ')[1].split('-')[1]);
+    let targetAnswerClass = targetClass.split(' ')[2];
 
-   
+    questions.forEach((question, index) => {
+        question.index = index;
+        
+        if (question.index === targetQuestionClass && question.correctAnswer === targetAnswerClass) {
+            return true;
+        } else {
+            return false;
+        }
+
+    })
 }
+
+
 
 const renderQuestionElement = (callback) => {
     setTimeout(() => {
@@ -115,3 +134,4 @@ play.addEventListener("click", (e) => {
     e.preventDefault();
     renderQuestionElement();
 });
+
